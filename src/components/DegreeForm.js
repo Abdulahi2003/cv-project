324962c28b35ac1472bd6b6c5182c4d5db6dc34d
Abdulahi <abdulahiaden444@gmail.com>
2024-08-15 22:07:24 -0400

    pass the state as props

---
+++ src/components/DegreeForm.js
@@ -0,0 +1,45 @@
+import React, { Component} from 'react'
+// import "../styles/forms.css"
+
+class DegreeForm extends Component {
+    render() {
+        const { handleChange, handleDelete, id, school, title, date } = this.props;
+
+        return(
+            <div className="degree-div">
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Name of School"
+                        onChange={handleChange}
+                        name="school"
+                        className={id}
+                        value={school}
+                        required
+                    ></input>
+                    <input
+                        type="text"
+                        placeholder="Title of Degree"
+                        onChange={handleChange}
+                        name="title"
+                        className={id}
+                        value={title}
+                        required
+                    ></input>
+                    <p className="form-labels">Graduation Date</p>
+                    <input 
+                    type="date"
+                    onChange={handleChange}
+                    name="date"
+                    className={id}
+                    value={date}
+                    required >
+                    </input>
+                </form>
+                <button className={id} onClick={handleDelete}>− DELETE</button>
+            </div>
+        )
+    }
+}
+
+export default DegreeForm;
